7b13d24188829b97c6c98d847ccb4346929de079
VineshAXL <csvinesh6@gmail.com>
2026-04-30 21:35:27 +0530

    Operators

---
+++ lesson2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class lesson2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a + b);   // Addition → 13
+        System.out.println(a - b);   // Subtraction → 7
+        System.out.println(a * b);   // Multiplication → 30
+        System.out.println(a / b);   // Division → 3 (not 3.33!)
+        System.out.println(a % b);   // Remainder → 1
+    }
+}
