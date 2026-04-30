ddfdf72e1602734263bfc1a735c60005914d9cc9
VineshAXL <csvinesh6@gmail.com>
2026-04-30 21:25:40 +0530

    Variables & Data Types

---
+++ lesson1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class lesson1 {
+    public static void main(String[] args) {
+
+        // Integer (whole numbers)
+        int age = 20;
+
+        // Decimal numbers
+        double price = 99.99;
+
+        // True or False
+        boolean isStudent = true;
+
+        // Single character
+        char grade = 'A';
+
+        // Text (called String in Java)
+        String name = "Ravi";
+
+        // Print them
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
